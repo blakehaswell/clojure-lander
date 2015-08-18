5106eca038af9332a01644b6c2e3870fb0e27f8b
Blake Haswell <haswell00@gmail.com>
2015-08-18 18:53:31 +0100

    Adds some prototype code to create windows, adds some constants, etc.

--- src/lander/core.clj
+++ src/lander/core.clj
@@ -1,16 +1,36 @@
 (ns lander.core
-  (:gen-class))
+  (:gen-class)
+  (:import (javax.swing JFrame)))
+
+;; This is how often the world is updated and rendered. We'll be aiming for 60
+;; frames per second (approx 16ms time-step).
+(def time-step (/ 1000 60))
 
 ;; Accelerations in m/s^2.
-(def gravity 9.8)
-(def vertical-thrust 20)
-(def horizontal-thrust 4)
+(def gravity-acceleration 9.8)
+(def thrust-acceleration 20)
+
+;; Rotation speed defined in degrees per second.
+(def rotation-speed 90)
+
+(def lander {:x 50 ; x and y coordinates defined in meters.
+             :y 50
+             :thrust true
+             :rotation 20
+             :vertical-speed -10
+             :horizontal-speed 2.4})
 
 (defn final-speed
   "Calculates a body's final speed (m/s), given an initial speed (m/s)
   and a constant acceleration (m/s^2) which is applied for time (s)."
   [initial-speed acceleration time]
   (+ initial-speed (* acceleration time)))
+
+(defn create-gui
+  []
+  (doto (JFrame. "Lander")
+    (.setSize 640 480)
+    (.setVisible true)))
 
 (defn -main
   "I don't do a whole lot ... yet."
